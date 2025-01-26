775c9d498821cb38e0fce9c85165162aaf7be182
MehdiAliPak <pakpak1251810@gmail.com>
2025-01-26 16:35:49 +0330

    fix(constants): change eitaa_token

--- application/config/constants.php
+++ application/config/constants.php
@@ -92,7 +92,7 @@ defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest auto
 |--------------------------------------------------------------------------
 */
 
-define('EITAA_TOKEN', '60930039:ETE~OTvqDq-vtgYq5EiuV-iAR]^7cJFU-FxSext?CbB-I)HQM4,3l7-rP@S8OnUAA-]2VnCV2t8m-8NqQBdrH).-d1[C1Z/krv-Gt0I1IxfkX-GgaDLiai@e-5%Ho0IYj[C-hYpylo.L3x-ZKflUj2M4S-hWTBfnR4aj-H2Vb');
+define('EITAA_TOKEN', '60930039:laKi6ig-Ml)Q8[?-EMpqNKn-UL(vPo}-dD7Xsx8-A%hpXLw-1WvuO4V-YxXSC9E-@v6g5mz-C5p*R7q-4KAfOhm-Oa~PsRi-oV9R^/T-eyunIYD-0%PfYWo-vHk{JF1-W1g7B,s-yYlYmIb-BZLs(V0-EUGJh');
 define('SECRET_KEY', '626f6f726f2062616261206d616e20696e6f20616e6a616d206e656d6964616d');
 define('CDN_URL', 'https://hls.zipak.info?path=');
 
