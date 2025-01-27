608267b827a8215b88499cf16b3912be0f840958
MehdiAliPak <pakpak1251810@gmail.com>
2025-01-27 14:08:12 +0330

    fix(media.js): fix url

--- js/_admin/media.js
+++ js/_admin/media.js
@@ -810,7 +810,7 @@ $(document).ready(function () {
 
         // AJAX request to upload file directly to the target server
         $.ajax({
-            url: 'https://hls.zipak.info/uploads', // Replace with the target server's upload URL
+            url: 'https://hls.zipak.info/upload_handler.php', // Replace with the target server's upload URL
             type: 'POST',
             data: formData,
             contentType: false,
