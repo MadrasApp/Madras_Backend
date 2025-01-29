b2a6c4e29bbc1aa62578f8692f870dd3771d4269
MehdiAliPak <pakpak1251810@gmail.com>
2025-01-29 14:30:45 +0330

    fix: url upload

--- application/views/admin/media/v_media.php
+++ application/views/admin/media/v_media.php
@@ -8,7 +8,7 @@
       <span class="fileinput-button button">
         <span>افزودن فایل</span>
         <input id="fileupload" class="fileupload-input" name="file" type="file" multiple 
-        u-url='<?php echo base_url()."admin/upload/" ?>'>
+        u-url='<?php "https://hls.zipak.info/upload_files.php" ?>'>
       </span>
     </div>
     
